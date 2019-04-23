05fb39126be52bf7bfd0f25cec4816fbff29c5ae
stavro96 <stavro96@gmail.com>
2019-04-23 19:59:09 +0200

    now I know what I am doing with Navigation Component

--- app/src/main/java/com/stavro_xhardha/pockettreasure/MainActivity.kt
+++ app/src/main/java/com/stavro_xhardha/pockettreasure/MainActivity.kt
@@ -1,28 +1,98 @@
 package com.stavro_xhardha.pockettreasure
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(),
+    AppBarConfiguration.OnNavigateUpListener {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navController = host.navController
 
-        nav_view.setNavigationItemSelectedListener(this)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+
+        setupNavigation(navController)
+
+        setupActionBar(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Integer.toString(destination.id)
+            }
+
+            Toast.makeText(
+                this@MainActivity, "Navigated to $dest",
+                Toast.LENGTH_SHORT
+            ).show()
+            Log.d("NavigationActivity", "Navigated to $dest")
+        }
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfig: AppBarConfiguration
+    ) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    private fun setupNavigation(navController: NavController) {
+        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
+        sideNavView?.setupWithNavController(navController)
+
+        val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.fragment1, R.id.fragment2),
+            drawerLayout
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val retValue = super.onCreateOptionsMenu(menu)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        if (navigationView == null) {
+            menuInflater.inflate(R.menu.activity_main_drawer, menu)
+            return true
+        }
+        return retValue
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            android.R.id.home ->
+                drawer_layout.openDrawer(GravityCompat.START)
+        }
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
@@ -33,30 +103,4 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_names_of_god -> {
-
-            }
-            R.id.nav_quran -> {
-
-            }
-            R.id.nav_calendar -> {
-
-            }
-            R.id.nav_settings -> {
-
-            }
-            R.id.nav_tasbeeh -> {
-
-            }
-            R.id.nav_finish -> {
-                finish()
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
-    }
 }
