0a9850f692a7147c7faf747863aa3e99955404d6
stavro96 <stavro96@gmail.com>
2019-06-14 19:35:47 +0200

    added something

--- app/src/main/java/com/stavro_xhardha/pockettreasure/PrayerAlarmReceiver.kt
+++ app/src/main/java/com/stavro_xhardha/pockettreasure/PrayerAlarmReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 import com.crashlytics.android.Crashlytics
 import com.stavro_xhardha.pockettreasure.brain.APPLICATION_TAG
 
@@ -12,5 +13,7 @@ class PrayerAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Crashlytics.log("ALARM RECEIVED at ${System.currentTimeMillis()}")
         Log.d(APPLICATION_TAG, "ALARM RECEIVED at ${System.currentTimeMillis()}")
+
+        Toast.makeText(context, "ESSELAMU ALEJKUM WA RAHMETULLAHI WA BEREKATUHU", Toast.LENGTH_LONG).show()
     }
 }
